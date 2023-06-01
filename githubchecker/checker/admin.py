fedaaf84699872d273549495d4cc1617cca0da13
Denis Krepak <xkrepak@fi.muni.cz>
2023-06-01 18:09:41 +0200

    Added docstring to admin.py

--- githubchecker/checker/admin.py
+++ githubchecker/checker/admin.py
@@ -1,20 +1,53 @@
 from django.contrib import admin
+
 from .models import Repository, Event, EventType, PullRequestMetrics
 
 
 class RepositoryAdmin(admin.ModelAdmin):
+    """
+    Admin class for the Repository model.
+
+    This class defines the admin interface configuration for the Repository model.
+
+    Attributes:
+    - list_display (tuple): The fields to be displayed in the admin list view.
+    """
     list_display = ('id', 'name', 'gh_repo_id', )
 
 
 class EventTypeAdmin(admin.ModelAdmin):
+    """
+    Admin class for the EventType model.
+
+    This class defines the admin interface configuration for the EventType model.
+
+    Attributes:
+    - list_display (tuple): The fields to be displayed in the admin list view.
+    """
     list_display = ('event_type', )
 
 
 class EventAdmin(admin.ModelAdmin):
+    """
+    Admin class for the Event model.
+
+    This class defines the admin interface configuration for the Event model.
+
+    Attributes:
+    - list_display (tuple): The fields to be displayed in the admin list view.
+    """
     list_display = ('id', 'event_type', 'created_at', )
 
 
 class PullRequestMetricsAdmin(admin.ModelAdmin):
+    """
+    Admin class for the PullRequestMetrics model.
+
+    This class defines the admin interface configuration for the PullRequestMetrics model.
+
+    Attributes:
+    - list_display (tuple): The fields to be displayed in the admin list view.
+    """
     list_display = ('gh_repo_id', 'respond', )
 
 
